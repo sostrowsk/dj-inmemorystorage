0bdb961f45d9c8c5c746e81860115b6ead7b2f63
Nick Presta <nick@nickpresta.ca>
2015-07-10 22:27:10 -0400

    Bump version to 1.4.0 -- Windows support

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ tests_require = requires
 setup(
     name="dj-inmemorystorage",
     description="A non-persistent in-memory data storage backend for Django.",
-    version="1.3.0",
+    version="1.4.0",
     url="https://github.com/waveaccounting/dj-inmemorystorage",
     license=open('LICENSE').read(),
     long_description=open('README.rst').read(),
